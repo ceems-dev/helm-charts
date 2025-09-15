c4b5c7b31531707cc6b3b48bf8086fae696809e1
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2025-09-15 16:59:21 +0200

    style: Format shell script in hack directory
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- charts/kube-ceems/hack/renovate-post-upgrade-hook.sh
+++ charts/kube-ceems/hack/renovate-post-upgrade-hook.sh
@@ -15,10 +15,10 @@ DASHBOARDS_DIR="$(dirname "${SCRIPT_DIR}")/files/dashboards"
 
 # Synchronize dashboards
 for DASH in "admin/cluster-status.json" "k8s/k8s-pod-summary.json" "k8s/k8s-single-pod-metrics.json" "openstack/os-single-vm-metrics.json" "openstack/os-vm-summary.json" "slurm/slurm-job-summary.json" "slurm/slurm-single-job-metrics.json"; do
-     FOLDER="$( cut -d '/' -f 1 <<< "$DASH" )"
-     mkdir -p "${DASHBOARDS_DIR}/${FOLDER}"
-     curl -sf -o "${DASHBOARDS_DIR}/${DASH}" "https://raw.githubusercontent.com/ceems-dev/ceems/${REF}/thirdparty/grafana/dashboards/${DASH}"
-     echo "Dashboard ${DASH} synchronized to ${DASHBOARDS_DIR}"
+	FOLDER="$(cut -d '/' -f 1 <<<"$DASH")"
+	mkdir -p "${DASHBOARDS_DIR}/${FOLDER}"
+	curl -sf -o "${DASHBOARDS_DIR}/${DASH}" "https://raw.githubusercontent.com/ceems-dev/ceems/${REF}/thirdparty/grafana/dashboards/${DASH}"
+	echo "Dashboard ${DASH} synchronized to ${DASHBOARDS_DIR}"
 done
 
 # Get helm-docs binary and update README based on template
