01d8918c3e31ed8cb10c5b5cde8d64b1ef668cae
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2025-09-22 14:05:48 +0200

    style: Fix super linter errors
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- charts/kube-ceems/hack/generate_readme.sh
+++ charts/kube-ceems/hack/generate_readme.sh
@@ -11,13 +11,13 @@ TMPDIR=$(mktemp -d /tmp/ceems_helm_charts.XXXXXX)
 # Get script directory
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
+# Move to root of chart folder
+cd "${SCRIPT_DIR}/../" || exit
+
 # Get helm-docs binary
 wget -O "${TMPDIR}/helm-docs_${HELM_DOCS_VERSION}_Linux_x86_64.tar.gz" "https://github.com/norwoodj/helm-docs/releases/download/v${HELM_DOCS_VERSION}/helm-docs_${HELM_DOCS_VERSION}_Linux_x86_64.tar.gz"
 tar -xvf "${TMPDIR}/helm-docs_${HELM_DOCS_VERSION}_Linux_x86_64.tar.gz" -C "${TMPDIR}"
 chmod +x "${TMPDIR}/helm-docs"
-
-# Move to root of chart folder
-cd "${SCRIPT_DIR}/../"
 
 # Generate README file from template
 "${TMPDIR}/helm-docs" --sort-values-order=file --template-files=.helm-docs/README.gotmpl
